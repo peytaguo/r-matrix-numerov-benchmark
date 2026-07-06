b599e6a927d1735a342c01b71c5db184ec553da3
Peyta Guo <peytaguo1@outlook.com>
2026-07-06 23:07:41 +1000

    Add c12_p potential

--- src/potentials.py
+++ src/potentials.py
@@ -7,8 +7,5 @@ E2 = 1.43996448  #units MeV fm
 #r in fm units
 #energies in MeV units. 
 
-def nuclear_potential(r):
-    return -73.8*math.exp(-(r/2.70)**2)
-
-def coulomb_potential(r):
-    return 6*E2/(r)
+def c12_p_potential(r);
+    return -73.8*math.exp(-(r/2.70)**2) + 6*E2/(r)
