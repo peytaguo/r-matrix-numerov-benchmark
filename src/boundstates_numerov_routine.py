305b8cfd34345de6a8ff7e37c097f5bf380c3bc8
Peyta Guo <peytaguo1@outlook.com>
2026-07-07 22:25:28 +1000

    Update boundstates_numerov_routine.py

--- src/boundstates_numerov_routine.py
+++ src/boundstates_numerov_routine.py
@@ -1,6 +1,70 @@
 import numpy as np
 
-from numerov import numerov_integrator, is_eval_checker, normalize_wavefunction
+from numerov import normalize_wavefunction
+
+def numerov_integrator(length, divisions, psi_0, psi_1, mass, potential, energy, full_integration):
+
+    """
+    Integrate the 1D Schrödinger equation using the Numerov method.
+
+    Parameters
+    ----------
+    length : float
+        Half-width of the integration interval [-length, length].
+    divisions : int
+        Number of grid intervals.
+    psi_0, psi_1 : float
+        Wavefunction values at the first two grid points.
+    """
+
+    if divisions % 2 != 0:
+        raise ValueError("divisions must be even so that x=0 is a grid point")
+    
+    if divisions < 2:
+        raise ValueError("divisions must be at least 2")
+
+    step_size = 2 * length / divisions
+    h2_12 = step_size * step_size / 12
+
+    grids = np.linspace(-length, length, divisions + 1)
+    
+    approximation = [0 for i in range(divisions + 1)]
+    approximation[0] = psi_0
+    approximation[1] = psi_1
+
+    gvals = np.array([g(x, mass, potential, energy) for x in grids])
+
+    if full_integration:
+        effective_division = divisions
+    else:
+        # one past midpoint: 
+        effective_division = divisions // 2 + 1
+    
+    for i in range(2, effective_division+1):
+            approximation[i] = updater(
+                gvals[i],
+                gvals[i-1], 
+                gvals[i-2], 
+                approximation[i-1], 
+                approximation[i-2], 
+                h2_12
+            )
+
+    return [grids, approximation[:effective_division + 1], full_integration, effective_division, step_size]
+
+
+def is_eval_checker(numerov_results):
+
+    approximation = numerov_results[1]
+    effective_division = numerov_results[3]
+    last_three = approximation[effective_division-2:effective_division+1]
+    step_size = numerov_results[4]
+
+    psi_0_approx = last_three[1]
+    dpsi_0_approx = (last_three[-1] - last_three[0]) / (2 * step_size)
+    criterion = 2 * dpsi_0_approx * psi_0_approx 
+
+    return [criterion, dpsi_0_approx]
 
 def routine(initial_guess, expected_spacing, iterations, length, divisions, psi_0, psi_1, mass, potential, tolerance):
     error_lst = []
